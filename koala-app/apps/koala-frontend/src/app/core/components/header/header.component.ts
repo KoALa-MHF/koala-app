0f5df1a33640f3c46780ab159a30891ea0168a66
TS-Daniel16 <daniel@tastysoft.de>
2022-10-31 09:52:33 +0100

    Added Toolbar to Header Component

--- koala-app/apps/koala-frontend/src/app/core/components/header/header.component.ts
+++ koala-app/apps/koala-frontend/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'koala-app-header',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly router: Router) {}
 
   ngOnInit(): void {}
+
+  public onToolbarSessionsPressed() {
+    this.router.navigate(['sessions']);
+  }
+
+  public onToolbarHomePressed() {
+    this.router.navigate(['']);
+  }
 }
--- koala-app/apps/koala-frontend/src/app/core/core.module.ts
+++ koala-app/apps/koala-frontend/src/app/core/core.module.ts
@@ -4,9 +4,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 
+//Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
   exports: [HeaderComponent, FooterComponent, HomeComponent],
 })
 export class CoreModule {}
