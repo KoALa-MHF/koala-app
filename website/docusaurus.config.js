7ac0e4196c17936455d4de683d455513c79f50bc
TS-Tino16 <tino@tastysoft.de>
2022-11-03 19:34:33 +0100

    Fixed website build

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/guide/intro',
               },
             ],
           },
