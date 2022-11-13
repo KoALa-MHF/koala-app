1462242ea634b3d925aea60a0c20ce3d523c0fa4
TS-Tino16 <tino@tastysoft.de>
2022-11-14 00:30:35 +0100

    Added setMedia mutation

--- koala-app/apps/api/src/app/sessions/sessions.resolver.ts
+++ koala-app/apps/api/src/app/sessions/sessions.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, ResolveField, Parent, Info } from '@nestjs/graphql';
 import { SessionsService } from './sessions.service';
 import { Session } from './entities/session.entity';
 import { CreateSessionInput } from './dto/create-session.input';
@@ -31,5 +31,10 @@ export class SessionsResolver {
   @Mutation(() => Session)
   removeSession(@Args('id', { type: () => Int }) id: number) {
     return this.sessionsService.remove(id);
+  }
+
+  @Mutation(() => Session)
+  async setMedia(@Args('id', { type: () => Int }) id: number, @Args('mediaId', { type: () => Int }) mediaId: number) {
+    return this.sessionsService.setMedia(id, mediaId);
   }
 }
