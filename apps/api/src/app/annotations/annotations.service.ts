e65af620490bb9af78c65ed4446d5a2a542be077
TS-Tino16 <116907083+TS-Tino16@users.noreply.github.com>
2022-11-20 22:41:48 +0100

    Added first version of annotations (#39)

---
+++ apps/api/src/app/annotations/annotations.service.ts
@@ -0,0 +1,58 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAnnotationInput } from './dto/create-annotation.input';
+import { UpdateAnnotationInput } from './dto/update-annotation.input';
+import { Annotation } from './entities/annotation.entity';
+
+@Injectable()
+export class AnnotationsService {
+
+  constructor(
+    @InjectRepository(Annotation)
+    private annotationsRepository: Repository<Annotation>
+  ) {}
+
+  create(createMarkerInput: CreateAnnotationInput) {
+    const newAnnotation = this.annotationsRepository.create({
+      start : createMarkerInput.start,
+      end : createMarkerInput.end,
+      marker : {
+        id : createMarkerInput.markerId
+      }
+    });
+
+    return this.annotationsRepository.save(newAnnotation);
+  }
+
+  findAll() {
+    return this.annotationsRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.annotationsRepository.findOneByOrFail({ id });
+  }
+
+  async update(id: number, updateAnnotationInput: UpdateAnnotationInput) {
+    try {
+      await this.annotationsRepository.update(id, {
+        start: updateAnnotationInput.start,
+        end: updateAnnotationInput.end,
+        marker: { 
+          id : updateAnnotationInput.markerId
+        }
+      });
+
+      return this.findOne(id);
+    } catch (error) {
+      throw new BadRequestException(error.detail);
+    }
+  }
+
+  remove(id: number) {
+    return this.annotationsRepository.delete(id);
+  }
+
+}
+
+
