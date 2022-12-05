141862adb93c6f5abcd6a844c95e791b9c358c6b
TS-Tino16 <116907083+TS-Tino16@users.noreply.github.com>
2022-12-05 22:02:56 +0100

    Added user session relation to annotation (#50)

--- apps/api/src/app/annotations/dto/update-annotation.input.ts
+++ apps/api/src/app/annotations/dto/update-annotation.input.ts
@@ -1,7 +1,9 @@
 import { CreateAnnotationInput } from './create-annotation.input';
-import { InputType, PartialType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateAnnotationInput extends PartialType(CreateAnnotationInput) {
-
+export class UpdateAnnotationInput extends PartialType(
+    OmitType(CreateAnnotationInput, ['userSessionId'] as const)
+  ) {
+    
 }
