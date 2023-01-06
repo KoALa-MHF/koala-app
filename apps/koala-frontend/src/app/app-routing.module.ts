b1b46c0042cbbca70e5a86199f5b0910a22e7f67
TS-Daniel16 <daniel@tastysoft.de>
2023-01-06 13:45:19 +0100

    Added Translation to Session Overview Page / Component

--- apps/koala-frontend/src/app/app-routing.module.ts
+++ apps/koala-frontend/src/app/app-routing.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './core/components/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'sessions',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
-  { 
+  {
     path: 'sessions',
-    loadChildren: () => import('./features/sessions/sessions.module').then(m => m.SessionsModule) 
-  }
+    loadChildren: () => import('./features/sessions/sessions.module').then((m) => m.SessionsModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- apps/koala-frontend/src/app/core/core.module.ts
+++ apps/koala-frontend/src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
 
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
@@ -14,7 +13,6 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
   ],
   imports: [
     CommonModule,
@@ -27,7 +25,6 @@ import { SharedModule } from '../shared/shared.module';
   exports: [
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
   ],
 })
 export class CoreModule {}
