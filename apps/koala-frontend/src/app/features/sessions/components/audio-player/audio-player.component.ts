b77fd31634c29bd4913958206f72e48b700c8d62
TS-Stefan <koala@Stefans-MBP.lan>
2023-02-25 16:30:40 +0100

    updates audio-control comp based on  mockups

--- apps/koala-frontend/src/app/features/sessions/components/audio-player/audio-player.component.ts
+++ apps/koala-frontend/src/app/features/sessions/components/audio-player/audio-player.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
-import { MediaActions, MediaControlService, MediaEvent } from '../../services/media-control.service';
+import { MediaActions, MediaEvent } from '../../services/media-control.service';
 
 @Component({
   selector: 'koala-app-audio-player',
@@ -8,22 +8,33 @@ import { MediaActions, MediaControlService, MediaEvent } from '../../services/me
     './audio-player.component.scss',
     '../../session-common.scss',
   ],
-  providers: [
-    MediaControlService,
-  ],
+  providers: [],
 })
 export class AudioPlayerComponent {
   @Input() metadata: MediaMetadata | undefined;
   @Output() mediaEvent: EventEmitter<MediaEvent> = new EventEmitter<MediaEvent>();
 
-  volume = 5;
-  constructor(public mc: MediaControlService) {}
+  muted = 0;
+  playing = false;
+  mutedIcon = 'pi pi-volume-up';
+  playIcon = 'pi pi-play';
 
   onPlay() {
+    if (!this.playing) {
+      this.playIcon = 'pi pi-pause';
+      this.playing = true;
+    } else {
+      this.playIcon = 'pi pi-play';
+      this.playing = false;
+    }
     this.mediaEvent.emit({ actions: MediaActions.Play });
   }
 
   onStop() {
+    if (this.playing) {
+      this.playIcon = 'pi pi-play';
+      this.playing = false;
+    }
     this.mediaEvent.emit({ actions: MediaActions.Stop });
   }
 
@@ -37,5 +48,16 @@ export class AudioPlayerComponent {
 
   onVolumeChange(volume: any) {
     this.mediaEvent.emit({ actions: MediaActions.VolumeChange, value: volume });
+  }
+
+  onMute() {
+    if (!this.muted) {
+      this.mutedIcon = 'pi pi-volume-off';
+      this.muted = 1;
+    } else {
+      this.mutedIcon = 'pi pi-volume-up';
+      this.muted = 0;
+    }
+    this.mediaEvent.emit({ actions: MediaActions.Mute, value: this.muted });
   }
 }
