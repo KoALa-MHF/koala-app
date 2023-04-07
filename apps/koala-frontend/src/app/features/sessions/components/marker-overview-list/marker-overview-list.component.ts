f3ea90b20769090bbd66bb66bbf1a9fad51862f9
TS-Daniel16 <daniel@tastysoft.de>
2023-04-07 11:17:05 +0200

    Added Search Field in Load Marker Dialog

--- apps/koala-frontend/src/app/features/sessions/components/marker-overview-list/marker-overview-list.component.ts
+++ apps/koala-frontend/src/app/features/sessions/components/marker-overview-list/marker-overview-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 import { Marker } from '../../types/marker.entity';
 
 @Component({
@@ -12,10 +13,20 @@ export class MarkerOverviewListComponent {
   @Input() markers!: Marker[];
   @Output() markerAdd = new EventEmitter<Marker[]>();
 
+  @ViewChild('markerOverviewTable') markerOverviewTable: Table | undefined;
   selectedMarkers: Marker[] = [];
 
   addMarker() {
     this.markerAdd.emit(this.selectedMarkers);
     this.selectedMarkers = [];
+  }
+
+  clear(table: Table, inputField: any) {
+    table.clear();
+    inputField.value = '';
+  }
+
+  applyFilterGlobal(event: any, stringVal: string) {
+    this.markerOverviewTable?.filterGlobal((event.target as HTMLInputElement).value, stringVal);
   }
 }
