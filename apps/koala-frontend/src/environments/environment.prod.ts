3240887659ca06e9e0be20641389fc60742d7c7b
TS-Daniel16 <daniel@tastysoft.de>
2023-05-16 21:58:38 +0200

    Switched to Secure WebSocket

--- apps/koala-frontend/src/environments/environment.prod.ts
+++ apps/koala-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://koala-app.de',
-  wsBaseUrl: 'ws://koala-app.de',
+  wsBaseUrl: 'wss://koala-app.de',
   samlUrl: 'https://koala-dev.onelogin.com/trust/saml2/http-post/sso/aa78b723-a1a4-46a1-ae46-cc2aa6427e94',
 };
