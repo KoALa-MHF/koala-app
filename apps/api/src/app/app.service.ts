f0df8ee509a153c63d468cb5870802a5d7dd6f0a
TS-Tino16 <tino@tastysoft.de>
2023-06-30 20:59:30 +0200

    Initial e2e test

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,9 @@ import { Message } from '@koala-app/api-interfaces';
 
 @Injectable()
 export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
   getData(): Message {
     return { message: 'Welcome to api!' };
   }
