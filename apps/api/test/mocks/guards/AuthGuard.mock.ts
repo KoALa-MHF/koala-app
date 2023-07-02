d5f752670abdfbec856b021e360ba59c7dc77b40
TS-Tino16 <tino@tastysoft.de>
2023-07-03 00:20:35 +0200

    Fixed user isRegistered method missing error

--- apps/api/test/mocks/guards/AuthGuard.mock.ts
+++ apps/api/test/mocks/guards/AuthGuard.mock.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '../../../src/app/core/guards/auth.guard';
 import { UsersData } from '../../../src/app/seed/data/users.data';
+import { User } from '../../../src/app/users/entities/user.entity';
 
 @Injectable()
 export class AuthGuardMock extends AuthGuard {
@@ -9,9 +10,15 @@ export class AuthGuardMock extends AuthGuard {
     if (req.headers.authorization) {
       const userId = req.headers.authorization.split(' ')[1];
 
-      req.user = Object.values(UsersData).find((user) => {
+      const authUser = Object.values(UsersData).find((user) => {
         return user.id == userId;
       });
+
+      if (authUser) {
+        const user = new User();
+        Object.assign(user, authUser);
+        req.user = user;
+      }
     }
     this.handleRequest(null, req.user, null, context);
     return true;
