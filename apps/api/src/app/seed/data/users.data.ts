fc7b0fe1763f5551afaad185ae74c422bfe64e15
TS-Tino16 <tino@tastysoft.de>
2023-07-02 15:31:29 +0200

    Updated Test Data

---
+++ apps/api/src/app/seed/data/users.data.ts
@@ -0,0 +1,24 @@
+export const UsersData = {
+  sessionOwner1: {
+    id: 1,
+    displayName: 'Session Owner 1',
+    email: 'session-owner-1@koala-app.de',
+    samlId: '123456',
+  },
+  sessionOwner2: {
+    id: 2,
+    displayName: 'Session Owner 2',
+    email: 'session-owner-2@koala-app.de',
+    samlId: '654321',
+  },
+  sessionParticipant1: {
+    id: 3,
+    displayName: 'Session Participant 1',
+    email: 'session-participant-1@koala-app.de',
+  },
+  sessionParticipant2: {
+    id: 4,
+    displayName: 'Session Participant 2',
+    email: 'session-participant-2@koala-app.de',
+  },
+};
--- apps/api/src/app/seed/seeders/sessions.seeder.ts
+++ apps/api/src/app/seed/seeders/sessions.seeder.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { Session } from '../../sessions/entities/session.entity';
-import { faker } from '@faker-js/faker';
 import { SeederInterface, SeederOptions } from '../seeder.interface';
 import { Toolbar } from '../../toolbars/entities/toolbar.entity';
+import { SessionsData } from '../data/sessions.data';
+import { UserSession } from '../../user-sessions/entities/user-session.entity';
 
 @Injectable()
 export class SessionsSeeder implements SeederInterface<Session> {
@@ -15,16 +16,24 @@ export class SessionsSeeder implements SeederInterface<Session> {
 
   async seed(options: SeederOptions): Promise<Session[]> {
     const sessions: Partial<Session>[] = [];
-    for (let i = 0; i < 3; i++) {
-      sessions.push(
-        this.sessionsRepository.create({
-          name: faker.commerce.productName(),
-          owner: options.users[i],
-          toolbars: [
-            new Toolbar(),
-          ],
-        })
-      );
+
+    for (let i = 0; i < SessionsData.length; i++) {
+      const sessionData = SessionsData[i];
+
+      const userSession = new UserSession();
+      userSession.ownerId = sessionData.owner.id;
+
+      const session = {
+        ...SessionsData[i],
+        toolbars: [
+          new Toolbar(),
+        ],
+        userSessions: [
+          userSession,
+        ],
+      } as Partial<Session>;
+
+      sessions.push(this.sessionsRepository.create(session));
     }
     return await this.sessionsRepository.save(sessions);
   }
