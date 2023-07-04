78de898657a7f83662ff5a42d52530af577ec4d3
TS-Tino16 <116907083+TS-Tino16@users.noreply.github.com>
2023-07-04 11:21:50 +0200

    Fixed missing globalPrefix error in main.ts (#274)

--- apps/api/src/bootstrap.ts
+++ apps/api/src/bootstrap.ts
@@ -3,11 +3,12 @@ import { GraphQLValidationPipe } from './app/core/pipes/graphql-validation.pipe'
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as MailDev from 'maildev';
 
+export const GLOBAL_PREFIX = 'api';
+
 export function setupApplication(app: INestApplication) {
   app.useGlobalPipes(new GraphQLValidationPipe());
   app.use(graphqlUploadExpress({ maxFileSize: 100000000, maxFiles: 10 }));
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix(GLOBAL_PREFIX);
 }
 
 export function setupMailDevelopmentServer() {
