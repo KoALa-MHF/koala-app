21cb4370cc8192df3ca09b39a32d688b016395d6
TS-Tino16 <116907083+TS-Tino16@users.noreply.github.com>
2023-07-19 09:36:57 +0200

    Corrected SAML URL call from Frontend (#338)

--- apps/koala-frontend/src/environments/environment.prod.ts
+++ apps/koala-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://koala.mh-freiburg.de',
   wsBaseUrl: 'wss://koala.mh-freiburg.de',
-  samlUrl: 'https://koala-dev.onelogin.com/trust/saml2/http-post/sso/aa78b723-a1a4-46a1-ae46-cc2aa6427e94',
+  samlUrl: 'https://koala.mh-freiburg.de/api/auth/sso/saml/login',
 };
