07e5cf40dcec48d7db4fa6d83a021ddf32f2ba6d
TS-Tino16 <tino@tastysoft.de>
2023-08-06 15:55:52 +0200

    Update media.resolver.ts

--- apps/api/src/app/media/media.resolver.ts
+++ apps/api/src/app/media/media.resolver.ts
@@ -2,12 +2,16 @@ import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { MediaService } from './media.service';
 import { Media } from './entities/media.entity';
 import { CreateMediaInput } from './dto/create-media.input';
+import { AuthGuard } from '../core/guards/auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { RegisteredUserGuard } from '../core/guards/registerd-user.guard';
 
 @Resolver(() => Media)
 export class MediaResolver {
   constructor(private readonly mediaService: MediaService) {}
 
   @Mutation(() => Media)
+  @UseGuards(AuthGuard, RegisteredUserGuard)
   createMedia(@Args('createMediaInput') createMediaInput: CreateMediaInput) {
     return this.mediaService.create(createMediaInput);
   }
