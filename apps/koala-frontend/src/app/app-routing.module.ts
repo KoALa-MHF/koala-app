f04cb84e5800c294173bdb24d74d4a663fb2d958
TS-Tino16 <116907083+TS-Tino16@users.noreply.github.com>
2023-08-07 22:29:37 +0200

    Added imprint and about page (#345)
    Co-authored-by: Daniel Heffter <99263623+TS-Daniel16@users.noreply.github.com>

--- apps/koala-frontend/src/app/app-routing.module.ts
+++ apps/koala-frontend/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './features/auth/guards/auth.guard';
 import { LayoutComponent } from './layout.component';
+import { ImprintPage } from './core/pages/imprint/imprint.page';
+import { AboutPage } from './core/pages/about/about.page';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'sessions',
     pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadChildren: () => import('./core/core.module').then((m) => m.CoreModule),
   },
   {
     path: 'sessions',
