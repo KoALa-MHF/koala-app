b2b8c2aabb214e7217bb73ce73c6969ddfd83245
TS-Tino16 <tino@tastysoft.de>
2023-08-07 22:13:35 +0200

    Enhanced test

--- apps/api/test/media.e2e-spec.ts
+++ apps/api/test/media.e2e-spec.ts
@@ -21,10 +21,22 @@ describe('Media (e2e) - REST', () => {
     });
 
     it('Authenticated user should get media file', async () => {
-      await supertest(app.getHttpServer())
+      const response = await supertest(app.getHttpServer())
         .get('/api/media/1')
         .auth(`${UsersData.sessionOwner1.id}`, { type: 'bearer' })
         .expect(HttpStatus.OK);
+
+      expect(response.headers['content-type']).toEqual('audio/mpeg');
+      expect(response.headers['content-length']).toEqual('158848');
+      expect(response.headers['cache-control']).toEqual('no-cache');
+      expect(response.body).toMatchSnapshot();
+    });
+
+    it('Authenticated user getting a media file which does not exist should return "Not Found"', async () => {
+      await supertest(app.getHttpServer())
+        .get('/api/media/does_not_exist')
+        .auth(`${UsersData.sessionOwner1.id}`, { type: 'bearer' })
+        .expect(HttpStatus.NOT_FOUND);
     });
   });
 });
