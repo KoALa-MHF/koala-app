a07f8cb4a1561b55ecccb1b9bcef3226ab3d7715
TS-Tino16 <tino@tastysoft.de>
2023-08-09 00:35:08 +0200

    Mocking jsonwebtoken verify function
    Mocking jsonwebtoken verify function, so that we can pass a none valid JWT token which represents the user ID, instead of mocking the AuthGuards. Mocking the JwtService is not sufficient, as passport is not using this service

--- apps/api/test/mocks/test.util.ts
+++ apps/api/test/mocks/test.util.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-
 import { AppModule } from '../../src/app/app.module';
 import { setupApplication } from '../../src/bootstrap';
-import { AuthGuardMock } from './guards/AuthGuard.mock';
-import { AuthGuard } from '../../src/app/core/guards/auth.guard';
 import { SeedModule } from '../../src/app/seed/seed.module';
 import { SeedService } from '../../src/app/seed/seed.service';
-import { JwtService } from '@nestjs/jwt';
-import { JwtServiceMock } from './services/JwtService.mock';
-import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
+import { jest } from '@jest/globals';
+
+// Mocking jsonwebtoken verify function, so that we can pass a none valid JWT token which represents the user ID
+jest.mock('jsonwebtoken', () => ({
+  verify: jest.fn((token, secretOrPublicKey, options, callback) => {
+    let done;
+
+    if (callback) {
+      done = callback;
+    } else {
+      done = function (err, data) {
+        if (err) throw err;
+        return data;
+      };
+    }
+
+    const userId = token;
+    return done(null, {
+      sub: userId,
+    });
+  }),
+}));
 
 export async function setupTestApplication(): Promise<INestApplication> {
   const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -17,14 +33,7 @@ export async function setupTestApplication(): Promise<INestApplication> {
       SeedModule,
       AppModule,
     ],
-  })
-    .overrideGuard(PassportAuthGuard('jwt'))
-    .useValue(new AuthGuardMock())
-    .overrideGuard(AuthGuard)
-    .useValue(new AuthGuardMock())
-    .overrideProvider(JwtService)
-    .useValue(new JwtServiceMock())
-    .compile();
+  }).compile();
 
   const app: INestApplication = moduleFixture.createNestApplication();
   setupApplication(app);
