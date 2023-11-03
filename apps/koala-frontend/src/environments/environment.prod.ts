881048f893fd3b9c4562d5f06265922608756846
Daniel Heffter <99263623+TS-Daniel16@users.noreply.github.com>
2023-11-03 22:58:58 +0100

    Fixed Missing /app/ URL Adjustments (#440)

--- apps/koala-frontend/src/environments/environment.prod.ts
+++ apps/koala-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://koala.mh-freiburg.de',
+  baseUrl: 'https://koala.mh-freiburg.de/app',
   wsBaseUrl: 'wss://koala.mh-freiburg.de',
   samlUrl: 'https://koala.mh-freiburg.de/api/auth/sso/saml/login',
 };
