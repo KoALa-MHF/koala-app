b485f61dac231231d231d16675f55a2f1d2699b5
TS-Tino16 <tino@tastysoft.de>
2024-03-09 17:23:35 +0100

    Make sure that the session status is not always reseted

--- apps/api/src/app/sessions/dto/create-session.input.ts
+++ apps/api/src/app/sessions/dto/create-session.input.ts
@@ -12,7 +12,7 @@ export class CreateSessionInput {
   @IsOptional()
   description?: string;
 
-  @Field(() => SessionStatus, { defaultValue: SessionStatus.IN_PREPARATION })
+  @Field(() => SessionStatus, { nullable: true })
   @IsEnum(SessionStatus)
   @IsOptional()
   status?: SessionStatus;
