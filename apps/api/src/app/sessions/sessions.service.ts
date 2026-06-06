264738c83180e266909568f39a1b7133ae275497
Daniel Heffter <daniel-heffter@gmx.de>
2026-06-06 19:13:37 +0200

    Fixed Session Updates Overwrites Media Reference

--- apps/api/src/app/sessions/sessions.service.ts
+++ apps/api/src/app/sessions/sessions.service.ts
@@ -123,7 +123,7 @@ export class SessionsService {
         console.log(err);
       });
     } else if (
-      updateSessionInput.mediaId !== null &&
+      updateSessionInput.mediaId != null &&
       session.mediaId != null &&
       updateSessionInput.mediaId !== session.mediaId
     ) {
