b2c1c920a72ea6a40ca821852f3591c03c9eaf96
Daniel Heffter <daniel-heffter@gmx.de>
2026-07-26 20:51:22 +0200

    Fixed Missing Annotations on Navigation
    Slider and Range annotations were not saved when video session was still running and a navigation was triggered. The deactivate guard now triggers the "normal" end of sliders and range annotations as it would on stop or end of video.

--- apps/koala-frontend/src/app/features/sessions/guards/session-unsaved-changes.guard.ts
+++ apps/koala-frontend/src/app/features/sessions/guards/session-unsaved-changes.guard.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 
 export interface BlockNavigationIfUnsavedChanges {
   hasUnsavedChanges(): boolean;
+  endActiveSliders(time?: number): void;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -13,11 +14,9 @@ export class UnsavedChangesGuard<T extends BlockNavigationIfUnsavedChanges> impl
   canDeactivate(component: T): Observable<boolean> | boolean {
     if (!component.hasUnsavedChanges()) {
       return true;
-    }
-    if (window.confirm(this.translateService.instant('SESSION.UNSAVED_CHNAGES_WARNING'))) {
-      return true;
     } else {
-      return false;
+      component.endActiveSliders();
+      return true;
     }
   }
 }
